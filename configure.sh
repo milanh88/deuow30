ad8979101cd8e35323fc68a4aa48c70695b3ff26
milanh88 <103425014+milanh88@users.noreply.github.com>
2022-09-23 07:00:39 +0000

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# V2Ray new configuration
+# Run V2ray
+# Write V2Ray configuration
+cat << EOF > /etc/v2ray/config.json
+{
+    "inbounds": [{
+        "port": 8081,
+        "protocol": "vmess",
+        "settings": {
+            "clients": [{
+                "id": "95beed92-9515-443b-aa02-aa68989dd418",
+                "alterId": 0
+            }]
+        },
+        "streamSettings": {
+            "network": "ws",
+            "wsSettings": {
+                "path": "/app"
+            }
+        }
+    }],
+    "outbounds": [{
+        "protocol": "freedom"
+    }]
+}
+EOF
+
+#Run V2ray
+
+/usr/bin/v2ray/v2ray run -config=/etc/v2ray/config.json
